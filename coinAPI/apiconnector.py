23661eee2d49f379a9584960fd959abac23f109d
Nayeem Kamal <nhk6@njit.edu>
2021-11-06 08:48:26 -0700

    added a thread to cache the api data every minute

--- coinAPI/apiconnector.py
+++ coinAPI/apiconnector.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-import pika
-import logging
-import json
-import threading
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
@@ -47,41 +42,3 @@ def cache_data():
             pp.pprint(data)
         sleep(60 - time() % 60)
 
-
-x = threading.Thread(target=cache_data)
-x.start()
-
-
-un='logreader'
-credentials = pika.PlainCredentials(un,un )
-parameters = pika.ConnectionParameters('192.168.194.195',
-                                       5672,
-                                       'it490',
-                                       credentials)
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
-
-channel.exchange_declare(exchange='NetworkLog', exchange_type='fanout')
-
-result = channel.queue_declare(queue='NetworkLog', exclusive=False)
-queue_name = result.method.queue
-
-channel.queue_bind(exchange='NetworkLog', queue=queue_name)
-
-print(' [*] Waiting for logs. To exit press CTRL+C')
-
-
-def log(body):
-    with open("network.log", 'a') as file1:
-        file1.write("\n%r" % body)
-
-
-def callback(ch, method, properties, body):
-    print(" [x] %r" % body)
-    log(body)
-
-
-channel.basic_consume(
-    queue=queue_name, on_message_callback=callback, auto_ack=True)
-
-channel.start_consuming()
