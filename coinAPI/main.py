33e588f212610c243d9b38edad92003601e1f2a5
Nayeem Kamal <nhk6@njit.edu>
2021-11-07 15:27:33 -0800

    working rpc client and server connected to crypto apis

--- coinAPI/main.py
+++ coinAPI/main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import pika
 import logging
 import json
@@ -6,94 +5,141 @@ import threading
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
-import pprint 
 import os
 from time import time, sleep
 import cryptocompare
 import datetime
 
 
-global data_cache
-data_cache={}
-global historical_data_cache
-historical_data_cache={}
 
-cryptocompare.cryptocompare._set_api_key_parameter('4445849e8c74ebfe227262a54942daedfd50da463d3777b068c667e6d4495b2a')
-historical_data_cache=cryptocompare.get_historical_price_day('BTC', 'USD', limit=1000, exchange='CCCAGG')
-with open("datahistory.json","w+") as datafile:
-    datafile.write(json.dumps(historical_data_cache))
-            
-print(historical_data_cache)
+global data_cache
+data_cache = {"data": {}}
+global historical_data_cache
+historical_data_cache = {}
+
+
+def loadData():
+    global data_cache
+    global historical_data_cache
+    cryptocompare.cryptocompare._set_api_key_parameter(
+        '4445849e8c74ebfe227262a54942daedfd50da463d3777b068c667e6d4495b2a')
+    historical_data_cache = cryptocompare.get_historical_price_day(
+        'BTC', 'USD', limit=1000, exchange='CCCAGG')
+    with open("datahistory.json", "w+") as datafile:
+        datafile.write(json.dumps(historical_data_cache))
+
+    url = f'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+    key = "092f78e5-80ec-4b38-8d06-c6b8b9d9d852"
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': key,
+    }
+    parameters = {'symbol': 'BTC,ETH'}
+
+    session = Session()
+    session.headers.update(headers)
+    try:
+            response = session.get(url, params=parameters)
+            data_cache = json.loads(response.text)
+
+            with open("data.json", "w+") as datafile:
+                datafile.write(json.dumps(data_cache))
+
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+            data = json.loads(response.text)
+loadData()
+
+
+
 def cache_data():
     global data_cache
-    while True:
-        # thing to run
+    global historical_data_cache
 
-        pp = pprint.PrettyPrinter(indent=4)
+    while True:
+
+        cryptocompare.cryptocompare._set_api_key_parameter(
+            '4445849e8c74ebfe227262a54942daedfd50da463d3777b068c667e6d4495b2a')
+        historical_data_cache = cryptocompare.get_historical_price_day(
+            'BTC', 'USD', limit=1000, exchange='CCCAGG')
+        with open("datahistory.json", "w+") as datafile:
+            datafile.write(json.dumps(historical_data_cache))
 
         url = f'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
-        print(url)
         key = "092f78e5-80ec-4b38-8d06-c6b8b9d9d852"
         headers = {
             'Accepts': 'application/json',
             'X-CMC_PRO_API_KEY': key,
         }
-        parameters = {
-        'symbol':'BTC,ETH'
-
-        }
+        parameters = {'symbol': 'BTC,ETH'}
 
         session = Session()
         session.headers.update(headers)
         try:
             response = session.get(url, params=parameters)
             data_cache = json.loads(response.text)
-            pp.pprint(data_cache)
 
-            with open("data.json","w+") as datafile:
+            with open("data.json", "w+") as datafile:
                 datafile.write(json.dumps(data_cache))
-            
 
         except (ConnectionError, Timeout, TooManyRedirects) as e:
             data = json.loads(response.text)
-            pp.pprint(data)
+            # logger.log("dmz",json.dumps(data))
         sleep(60 - time() % 60)
 
 
 x = threading.Thread(target=cache_data)
 x.start()
+sleep(1)
 
 
-# un='logreader'
-# credentials = pika.PlainCredentials(un,un )
-# parameters = pika.ConnectionParameters('192.168.194.195',
-#                                        5672,
-#                                        'it490',
-#                                        credentials)
-# connection = pika.BlockingConnection(parameters)
-# channel = connection.channel()
 
-# channel.exchange_declare(exchange='NetworkLog', exchange_type='fanout')
+def getCurrentPrices():
+    global data_cache
+    ret = {"BTC": data_cache["data"]["BTC"]["quote"]["USD"]["price"], "ETH":data_cache["data"]["ETH"]["quote"]["USD"]["price"]}
 
-# result = channel.queue_declare(queue='NetworkLog', exclusive=False)
-# queue_name = result.method.queue
-
-# channel.queue_bind(exchange='NetworkLog', queue=queue_name)
-
-# print(' [*] Waiting for logs. To exit press CTRL+C')
+    return ret
+def getMonthlyHistorical():
+    ret={}
+    return ret
 
 
-# def log(body):
-#     with open("network.log", 'a') as file1:
-#         file1.write("\n%r" % body)
+
+un="dmz"
+queuename="dmz"
+credentials = pika.PlainCredentials(un,un )
+parameters = pika.ConnectionParameters('192.168.194.195',5672,'it490',credentials)
+connection = pika.BlockingConnection(parameters)
 
 
-# def callback(ch, method, properties, body):
-#     print(" [x] %r" % body)
-#     log(body)
+channel = connection.channel()
 
+channel.queue_declare(queue=queuename,durable=True)
 
-# channel.basic_consume(
-#     queue=queue_name, on_message_callback=callback, auto_ack=True)
+def on_request(ch, method, props, body):
+    print(body)
+    n = json.loads(body)
+    print("%s" % str(n))
+    if(n["function"]=="getCurrentPrices"):
+        response = getCurrentPrices()
+    else:
+        response="not parsed"
+    print(response)
 
-# channel.start_consuming()
+    ch.basic_publish(exchange='dmz',
+                     routing_key="*",
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         props.correlation_id),
+                     body=str(response))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue=queuename, on_message_callback=on_request)
+
+channel_close = channel.is_closed
+channel_open = channel.is_open
+print("channel is_closed ", channel_close)
+print("channel is_open ", channel_open)
+
+print(" [x] Awaiting RPC requests")
+channel.start_consuming()
+print("consuming")
